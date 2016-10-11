3e6a3e456bb02794f86dc7747ba1d2072a9e87b3
ewgenius <ewgeniux@gmail.com>
2016-10-11 14:33:45 +0300

    upd

--- src/ScrollLens.tsx
+++ src/ScrollLens.tsx
@@ -2,17 +2,45 @@ import * as React from 'react'
 import { Component, Props, HTMLAttributes } from 'react'
 
 export interface ScrollLensProps extends HTMLAttributes<HTMLDivElement>, Props<ScrollLens> {
+  itemHeight?: number
+  items: any[]
+}
+
+export interface ScrollLensState {
 
 }
 
-export class ScrollLens extends Component<ScrollLensProps, {}> {
+export class ScrollLens extends Component<ScrollLensProps, ScrollLensState> {
+  static defaultProps = {
+    itemHeight: 10
+  }
+
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  onScroll() {
+
+  }
+
+  renderItems(): JSX.Element {
+    return <b>222</b>
+  }
+
   render() {
     const {style, className, id} = this.props
     return <div
       id={id}
       className={className}
-      style={style}>
-      {this.props.children}
+      style={style}
+      onScroll={() => this.onScroll()}
+      ref='scroller'>
+      <div ref='container'>
+        <div ref='visible'>
+          {this.renderItems()}
+        </div>
+      </div>
     </div>
   }
 }
--- stories/index.js
+++ stories/index.js
@@ -7,6 +7,5 @@ import {
 import ScrollLens from '../dist';
 
 storiesOf('ScrollLens', module)
-  .add('simple', () => ( <
-    ScrollLens > < /ScrollLens>
-  ));
+  .add('empty', () => (<ScrollLens />))
+  .add('simple', () => (<ScrollLens items={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} />));
