e58bf9b866a14104434c7aebcd93b2d7e2e80156
ewgenius <ewgeniux@gmail.com>
2016-10-11 14:17:27 +0300

    remove custom

--- .storybook/stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@kadira/storybook'
-import ScrollLens from '../dist'
-
-storiesOf('Button', module)
-  .add('default view', () => (
-    <ScrollLens>test</ScrollLens>
-  ))
